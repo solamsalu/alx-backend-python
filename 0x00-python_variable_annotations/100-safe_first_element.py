df08b4c577beb95213c21eaa364f2cc4021df6e6
Solomon Amsalu <solamsalu@gmail.com>
2023-10-05 23:20:54 +0300

    100 update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,6 +7,6 @@ def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """ Type-annotated function safe_first_element that takes a iterable argument.
     """
     if lst:
-        return lst[0]
+      return lst[0]
     else:
       return None
