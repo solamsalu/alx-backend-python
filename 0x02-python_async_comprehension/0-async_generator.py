af15263815bf2f348c32562065f670cadccb8fac
Solomon Amsalu <solamsalu@gmail.com>
2023-10-10 21:55:51 +0300

    Task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Task 0's module """
+from asyncio import sleep
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """  Async Generator  """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
