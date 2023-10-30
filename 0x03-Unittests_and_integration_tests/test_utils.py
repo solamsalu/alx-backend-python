54e60efeeb5729e54ed089862307ca7806df65f5
Solomon Amsalu <solamsalu@gmail.com>
2023-10-30 15:55:30 +0300

    Task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,20 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(cm.exception.args[0], path[-1])
 
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_get.return_value.json.return_value = test_payload
+
+        response = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(response, test_payload)
+
 if __name__ == "__main__":
     unittest.main()
 
